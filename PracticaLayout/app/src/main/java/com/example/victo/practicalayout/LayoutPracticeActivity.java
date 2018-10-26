5f44b76051c1d752022b3a9290c45279551838a7
VictorD07 <victordiazmateo@gmail.com>
2018-10-26 14:22:20 -0400

    Practica de Layouts Mixtos

---
+++ PracticaLayout/app/src/main/java/com/example/victo/practicalayout/LayoutPracticeActivity.java
@@ -0,0 +1,13 @@
+package com.example.victo.practicalayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutPracticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_practice);
+    }
+}
